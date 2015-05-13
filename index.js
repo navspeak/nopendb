6fbb03072942a51ea7172df6c840de1f69276f1c
unknown <knavneet@NBHYDKNAVNEET7.bedford.progress.com>
2015-05-13 15:51:56 +0530

    Signed-off-by: unknown <knavneet@NBHYDKNAVNEET7.bedford.progress.com>

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require("./lib/nopendb.js");
+var path = require('path');
+var fs = require('fs');
+var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+console.log(lib);
+module.exports = require(lib+'nopendb.js');
